ca1e9a70128692fe8b40477b6606d96d25a79c18
Acer <kulna>
2022-05-16 10:56:09 +0600

    init: start home work.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+////////Звдвча 1/////////
+let name1 = 'camelCase';
+let name2 = 'snake_case';
+let name3 = 'PascalCase';
+alert(`${name1} ${name2} ${name3}`);
+
+
+////////Звдвча 2/////////
+let str1 = 'Салам';
+let str2 = 'Бишкек';
+alert(`${str1} ${str2}`);
+
+////////Звдвча 3/////////
+let name = 'Janat';
+alert(`Привет ${name}`);
+
+////////Звдвча 4/////////
+let age = 26;
+alert(`Мне ${age} лет`);
